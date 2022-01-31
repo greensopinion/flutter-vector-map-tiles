7c1340fa54639af05c892b49445bd089b863b48f
David Green <greensopinion@gmail.com>
2022-01-30 16:07:50 -0800

    experiment with a delayed symbol layer
    Experiment with rendering symbols in a
    separate layer, on a delay. This makes
    renders appear separately from tiles
    with a short delay of about 1s. By
    rendering symbols later, rendering frame
    rate is higher when zooming and initially
    rendering a tile.

--- lib/src/grid/grid_vector_tile.dart
+++ lib/src/grid/grid_vector_tile.dart
@@ -35,16 +35,27 @@ class GridVectorTileBody extends StatefulWidget {
 }
 
 class _GridVectorTileBodyState extends DisposableState<GridVectorTileBody> {
-  final VectorTileModel _model;
+  final VectorTileModel model;
   late final _VectorTilePainter _painter;
+  _VectorTilePainter? _symbolPainter;
 
-  _GridVectorTileBodyState(this._model);
+  _GridVectorTileBodyState(this.model);
 
   @override
   void initState() {
     super.initState();
-    _painter = _VectorTilePainter(_model);
-    _model.addListener(() {
+    final symbolTheme = model.symbolTheme;
+    _painter = _VectorTilePainter(_TileLayerOptions(model, model.theme,
+        renderMode: symbolTheme != null ? RenderMode.vector : model.renderMode,
+        paintBackground: model.paintBackground,
+        showTileDebugInfo: model.showTileDebugInfo));
+    if (symbolTheme != null) {
+      _symbolPainter = _VectorTilePainter(_TileLayerOptions(model, symbolTheme,
+          renderMode: RenderMode.vector,
+          paintBackground: false,
+          showTileDebugInfo: false));
+    }
+    model.addListener(() {
       if (!disposed) {
         setState(() {});
       }
@@ -54,30 +65,100 @@ class _GridVectorTileBodyState extends DisposableState<GridVectorTileBody> {
   @override
   void dispose() {
     super.dispose();
-    _model.dispose();
+    model.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return RepaintBoundary(
+    final tile = RepaintBoundary(
         key: Key(
             'tileBodyBoundary${widget.model.tile.z}_${widget.model.tile.x}_${widget.model.tile.y}'),
         child: CustomPaint(painter: _painter));
+    final symbolPainter = _symbolPainter;
+    if (symbolPainter != null) {
+      return Stack(fit: StackFit.expand, children: [
+        tile,
+        _DelayedPainter(
+            key: Key(
+                'delayedSymbols${widget.model.tile.z}_${widget.model.tile.x}_${widget.model.tile.y}'),
+            painter: symbolPainter)
+      ]);
+    }
+    return tile;
   }
+}
+
+class _DelayedPainter extends material.StatefulWidget {
+  final _VectorTilePainter painter;
+
+  const _DelayedPainter({material.Key? key, required this.painter})
+      : super(key: key);
+  @override
+  material.State<material.StatefulWidget> createState() {
+    return _DelayedPainterState(painter);
+  }
+}
+
+class _DelayedPainterState extends DisposableState<_DelayedPainter> {
+  late final ScheduledDebounce debounce;
+  final _VectorTilePainter painter;
+  var _render = false;
+
+  _DelayedPainterState(this.painter) {
+    debounce = ScheduledDebounce(_notifyUpdate,
+        delay: Duration(milliseconds: 800),
+        jitter: Duration(milliseconds: 100),
+        maxAge: Duration(seconds: 10));
+    painter.options.model.addListener(() {
+      debounce.update();
+    });
+  }
+
+  @override
+  material.Widget build(material.BuildContext context) {
+    if (!_render) {
+      debounce.update();
+      return material.Container();
+    }
+    _render = false;
+    return RepaintBoundary(
+        key: Key(
+            'tileBodyBoundarySymbols${painter.options.model.tile.z}_${painter.options.model.tile.x}_${painter.options.model.tile.y}'),
+        child: CustomPaint(painter: painter));
+  }
+
+  void _notifyUpdate() {
+    if (!disposed) {
+      setState(() {
+        _render = true;
+      });
+    }
+  }
+}
+
+class _TileLayerOptions {
+  final VectorTileModel model;
+  final RenderMode renderMode;
+  final Theme theme;
+  final bool paintBackground;
+  final bool showTileDebugInfo;
+
+  _TileLayerOptions(this.model, this.theme,
+      {required this.renderMode,
+      required this.paintBackground,
+      required this.showTileDebugInfo});
 }
 
 enum _PaintMode { vector, raster, background, none }
 
 class _VectorTilePainter extends CustomPainter {
-  final VectorTileModel model;
+  final _TileLayerOptions options;
   TileIdentity? _lastPaintedId;
   var _lastPainted = _PaintMode.none;
   var _paintCount = 0;
   late final ScheduledDebounce debounce;
 
-  _VectorTilePainter(VectorTileModel model)
-      : this.model = model,
-        super(repaint: model) {
+  _VectorTilePainter(this.options) : super(repaint: options.model) {
     debounce = ScheduledDebounce(_notifyIfNeeded,
         delay: Duration(milliseconds: 100),
         jitter: Duration(milliseconds: 100),
@@ -86,12 +167,13 @@ class _VectorTilePainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    final model = options.model;
     if (model.disposed) {
       return;
     }
     bool changed = model.updateRendering();
     if (model.tileset == null && model.image == null) {
-      if (model.paintBackground) {
+      if (options.paintBackground) {
         _paintBackground(canvas, size);
       }
       return;
@@ -99,7 +181,7 @@ class _VectorTilePainter extends CustomPainter {
     final image = model.image;
     final renderImage = (changed ||
             model.tileset == null ||
-            (model.renderMode == RenderMode.mixed &&
+            (options.renderMode == RenderMode.mixed &&
                 (_lastPainted == _PaintMode.background ||
                     _lastPainted == _PaintMode.none))) &&
         image != null;
@@ -117,12 +199,12 @@ class _VectorTilePainter extends CustomPainter {
       canvas.drawImage(image!, Offset.zero, Paint());
       _lastPainted = _PaintMode.raster;
       _lastPaintedId = translation.translated;
-      if (model.renderMode == RenderMode.mixed) {
+      if (options.renderMode == RenderMode.mixed) {
         debounce.update();
       }
     } else {
       final tileClip = tileSizer.tileClip(size, tileSizer.effectiveScale);
-      Renderer(theme: model.theme).render(canvas, model.tileset!,
+      Renderer(theme: options.theme).render(canvas, model.tileset!,
           clip: tileClip,
           zoomScaleFactor: tileSizer.effectiveScale,
           zoom: model.lastRenderedZoom);
@@ -135,13 +217,14 @@ class _VectorTilePainter extends CustomPainter {
   }
 
   void _paintBackground(Canvas canvas, Size size) {
+    final model = options.model;
     final tileSizer = GridTileSizer(model.defaultTranslation,
         model.zoomScaleFunction(model.tile.z), size, false, null);
     canvas.save();
     canvas.clipRect(Offset.zero & size);
     tileSizer.apply(canvas);
     final tileClip = tileSizer.tileClip(size, tileSizer.effectiveScale);
-    Renderer(theme: model.theme).render(canvas, Tileset({}),
+    Renderer(theme: options.theme).render(canvas, Tileset({}),
         clip: tileClip,
         zoomScaleFactor: tileSizer.effectiveScale,
         zoom: model.lastRenderedZoom);
@@ -154,7 +237,7 @@ class _VectorTilePainter extends CustomPainter {
 
   void _paintTileDebugInfo(Canvas canvas, Size size, bool renderedImage,
       double scale, GridTileSizer tileSizer) {
-    if (model.showTileDebugInfo) {
+    if (options.showTileDebugInfo) {
       ++_paintCount;
       final paint = Paint()
         ..strokeWidth = 2.0
@@ -172,7 +255,7 @@ class _VectorTilePainter extends CustomPainter {
           text: TextSpan(
               style: textStyle,
               text:
-                  '${model.tile}\nscale=$roundedScale\npaintCount=$_paintCount'),
+                  '${options.model.tile}\nscale=$roundedScale\npaintCount=$_paintCount'),
           textAlign: TextAlign.start,
           textDirection: TextDirection.ltr)
         ..layout();
@@ -183,19 +266,21 @@ class _VectorTilePainter extends CustomPainter {
   void _notifyIfNeeded() {
     Future.microtask(() {
       if (_lastPainted != _PaintMode.vector) {
-        model.requestRepaint();
+        options.model.requestRepaint();
       }
     });
   }
 
   @override
   bool shouldRepaint(covariant _VectorTilePainter oldDelegate) =>
-      model.hasChanged() ||
+      options.model.hasChanged() ||
       (oldDelegate._lastPainted == _PaintMode.raster &&
-          (oldDelegate._lastPaintedId != model.imageTranslation?.translated ||
-              model.translation != null)) ||
+          (oldDelegate._lastPaintedId !=
+                  options.model.imageTranslation?.translated ||
+              options.model.translation != null)) ||
       (oldDelegate._lastPainted == _PaintMode.vector &&
-          oldDelegate._lastPaintedId != model.translation?.translated) ||
+          oldDelegate._lastPaintedId !=
+              options.model.translation?.translated) ||
       (oldDelegate._lastPainted == _PaintMode.background);
 }
 
