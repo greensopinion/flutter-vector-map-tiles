cbf689c9b2584df9136d7e0cc69b5dbfb22ac00c
David Green <greensopinion@gmail.com>
2024-02-18 16:48:53 -0800

    expose Retryable

--- lib/vector_map_tiles.dart
+++ lib/vector_map_tiles.dart
@@ -2,6 +2,7 @@ library vector_map_tiles;
 
 export 'src/provider/memory_vector_tile_provider.dart';
 export 'src/provider/network_vector_tile_provider.dart';
+export 'src/provider_exception.dart' show ProviderException, Retryable;
 export 'src/style/style.dart';
 export 'src/theme_extensions.dart';
 export 'src/tile_identity.dart';
@@ -10,4 +11,3 @@ export 'src/tile_providers.dart';
 export 'src/vector_tile_layer.dart';
 export 'src/vector_tile_layer_mode.dart';
 export 'src/vector_tile_provider.dart';
-export 'src/provider_exception.dart' show ProviderException;
