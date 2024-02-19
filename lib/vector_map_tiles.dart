bfbdd3c3230e042f577d675be08713400cf6de72
David Green <greensopinion@gmail.com>
2024-02-18 16:45:59 -0800

    expose ProviderException

--- lib/vector_map_tiles.dart
+++ lib/vector_map_tiles.dart
@@ -10,3 +10,4 @@ export 'src/tile_providers.dart';
 export 'src/vector_tile_layer.dart';
 export 'src/vector_tile_layer_mode.dart';
 export 'src/vector_tile_provider.dart';
+export 'src/provider_exception.dart' show ProviderException;
