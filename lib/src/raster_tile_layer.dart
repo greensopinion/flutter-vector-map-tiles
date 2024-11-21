5047c3a70afc908a461703ba486097a9230b56fd
David Green <greensopinion@gmail.com>
2024-11-20 18:30:53 -0800

    reload raster tile layer if parameters change
    Facilitates changes to the map style,
    e.g. when switching to/from dark mode.

--- lib/src/raster_tile_layer.dart
+++ lib/src/raster_tile_layer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 
@@ -22,6 +23,7 @@ class RasterTileLayer extends StatefulWidget {
 class _RasterTileLayer extends State<RasterTileLayer> {
   late IsolateTileLoader _tileLoader;
   late TileProvider _tileProvider;
+  var _generation = 0;
 
   @override
   void initState() {
@@ -33,9 +35,26 @@ class _RasterTileLayer extends State<RasterTileLayer> {
   }
 
   @override
+  void didUpdateWidget(RasterTileLayer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (!mapEquals(oldWidget.entrypointParamters, widget.entrypointParamters)) {
+      final oldLoader = _tileProvider;
+      setState(() {
+        ++_generation;
+        oldLoader.dispose();
+        _tileLoader = IsolateTileLoader(
+            entrypoint: widget.entrypoint,
+            entrypointParamters: widget.entrypointParamters);
+        _tileProvider = _tileLoader.provider();
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return TileLayer(
-        key: Key('${widget.key}_tileLayer'), tileProvider: _tileProvider);
+        key: Key('${widget.key}_tileLayer_$_generation'),
+        tileProvider: _tileProvider);
   }
 
   @override
