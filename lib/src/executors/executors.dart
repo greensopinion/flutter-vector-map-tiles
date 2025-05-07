079f854301a3cdfef08037eed310cb7063b92de1
David Green <greensopinion@gmail.com>
2025-05-06 18:49:38 -0700

    fix broken web support (#239)

---
+++ lib/src/executors/executors.dart
@@ -0,0 +1,1 @@
+export 'executors_web.dart' if (dart.library.io) 'executors_std.dart';
---
+++ lib/src/executors/executors_std.dart
@@ -0,0 +1,4 @@
+import 'package:executor_lib/executor_lib.dart';
+
+Executor newConcurrentExecutor({required int concurrency}) =>
+    PoolExecutor(concurrency: concurrency);
